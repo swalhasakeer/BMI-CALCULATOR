0be3f5a809cdf2155f5393d90d188661858f569f
hibaanwer07 <hibaanwer0702@gmail.com>
2025-05-13 12:43:07 +0530

    files added

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Simple BMI Calculator")
+
+height = st.number_input("Enter your height (in cm):")
+weight = st.number_input("Enter your weight (in kg):")
+
+if st.button("Calculate BMI"):
+    bmi = weight / ((height / 100) ** 2)
+    st.write(f"Your BMI is: {bmi:.2f}")
